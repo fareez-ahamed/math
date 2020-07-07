55f2e32ebea5bf55e0742e0dd2a92c78637617ba
Fareez Ahamed <fareez.ahamed@gmail.com>
2020-07-07 22:48:27 +0530

    Bismillah

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/fareez-ahamed/math
+
+go 1.14
---
+++ math.go
@@ -0,0 +1,57 @@
+package math
+
+// MinInt returns minimum of two numbers
+func MinInt(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// MinUint returns minimum of two numbers
+func MinUint(a, b uint) uint {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// MaxInt returns minimum of two numbers
+func MaxInt(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// MaxUint returns minimum of two numbers
+func MaxUint(a, b uint) uint {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// BoundInt checks the int against boundary
+func BoundInt(val, min, max int) int {
+	switch {
+	case val > max:
+		return max
+	case val < min:
+		return min
+	default:
+		return val
+	}
+}
+
+// BoundUnt checks the int against boundary
+func BoundUnt(val, min, max uint) uint {
+	switch {
+	case val > max:
+		return max
+	case val < min:
+		return min
+	default:
+		return val
+	}
+}
